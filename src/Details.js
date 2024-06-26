41b479cab7935d3a03d3e91cd1f9b75395720166
Dhiandra <d.adithya.a@gmail.com>
2024-06-26 20:54:15 +0700

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,172 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Dhiandra Adithya Agsatra",
+  tagline: "I build things for web and app testing",
+  img: profile,
+  about: `Hi! I'm Dhiandra, a QA Engineer with 2+ years of experience in testing web and mobile apps. Proficient in all phases of the SDLC. My expertise spans both manual and automated testing, ensuring comprehensive coverage and efficiency. I plan, design, execute tests, and report defects. Passionate about engaging with and effectively communicating within cross-functional teams. I am dedicated to ensuring the software functions smoothly, meets requirements, and exceeds user expectations.  Let's embark on this journey together and redefine success!`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/dhiandraaa",
+  github: "https://www.github.com/dhiandraaa",
+  twitter: "https://twitter.com/abcdhiandra",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "QA Engineer",
+    Company: `AssetFindr`,
+    Location: "Jakarta, Indonesia",
+    Type: "Full Time",
+    Duration: "Jun 2024 - Present",
+  },
+  {
+    Position: "QA Engineer",
+    Company: `Mamikos`,
+    Location: "Jakarta, Indonesia",
+    Type: "Full Time",
+    Duration: "Jan 2023 - Mei 2024",
+  },
+  {
+    Position: "Quality Engineer",
+    Company: `Farmacare`,
+    Location: "Denpasar, Bali",
+    Type: "Full Time",
+    Duration: "Aug 2022 - Jan 2023",
+  },
+  {
+    Position: "Quality Assurance",
+    Company: `Mamikos`,
+    Location: "Remote",
+    Type: "Internship",
+    Duration: "Feb 2022 - Aug 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor in Computer Science",
+    Company: `Lampung University`,
+    Location: "Lampung",
+    Type: "GPA: 3,64",
+    Duration: "Aug 2016 - Jan 2021",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  vscode: vscode,
+  postman: postman,
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "MIFX",
+    image: projectImage1,
+    description: `This is an automation project for MIFX website`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Orderfaz",
+    image: projectImage2,
+    description: `This is an automation project for Orderfaz website`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Equalsys",
+    image: projectImage3,
+    description: `This is an automation project for Equalsys website`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "hi@example.com",
+  phone: "+91 12345 67890",
+};
